08b0048fb23f91654999e94b877ccfaf18021886
Albertojserr <alberto.j.serrano@gmail.com>
2022-10-23 20:37:08 +0200

    aprendizaje a clases

--- codigo/aprendizaje_no_supervisado.py
+++ codigo/aprendizaje_no_supervisado.py
@@ -19,39 +19,48 @@
 #-----------------------------------------------------------------------------------------
 
 
-
 import pandas as pnd
 import matplotlib.pyplot as plt
 import os
+
+from sklearn.cluster import KMeans
+
+from sklearn import mixture
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 print(dir_path)
 #para cogerlo de otra carpeta usamos lo siguiente
 #dir_path=dir_path+"/../teoria"
 #Caraga de datos
-frutas = pnd.read_csv(dir_path+"/datas/frutas.csv", names=['DIAMETRO','PESO'], header=None)
-
+class Aprendizaje:
+    def __init__(self):
+        self.frutas = pnd.read_csv(dir_path+"/datas/frutas.csv", names=['DIAMETRO','PESO'], header=None)
+    def vis(self):
 #Visualización gráfica de los datos
-frutas.plot.scatter(x="DIAMETRO",y="PESO")
-plt.show()
+        self.frutas.plot.scatter(x="DIAMETRO",y="PESO")
+        plt.title("Frutas del dataset")
+        plt.show()
 
 
-#Aprendizaje con el algoritmo K-Mean
-from sklearn.cluster import KMeans
-modelo=KMeans(n_clusters=2)
-modelo.fit(frutas)
+        #Aprendizaje con el algoritmo K-Mean
 
-#Predicciones
-predicciones_kmeans = modelo.predict(frutas)
+    def aprKMeans(self):
+        self.modelo=KMeans(n_clusters=2)
+        self.modelo.fit(self.frutas)
 
-#Visualización de la clusterización
-plt.scatter(frutas.DIAMETRO, frutas.PESO, c=predicciones_kmeans, s=50, cmap='viridis')
-plt.xlabel("DIAMETRO")
-plt.ylabel("PESO")
+        #Predicciones
+        predicciones_kmeans = self.modelo.predict(self.frutas)
 
-#Visualización de los centroides
-centers = modelo.cluster_centers_
-plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
-plt.show()
+        #Visualización de la clusterización
+        plt.scatter(self.frutas.DIAMETRO, self.frutas.PESO, c=predicciones_kmeans, s=50, cmap='viridis')
+        plt.xlabel("DIAMETRO")
+        plt.ylabel("PESO")
+
+        #Visualización de los centroides
+        centers = self.modelo.cluster_centers_
+        plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+        plt.title("Centroides")
+        plt.show()
 
 #Guardar el modelo (eliminar marca de comentario # si es necesario)
 #from joblib import dump
@@ -61,50 +70,65 @@ plt.show()
 #CEREZA: 26,98 mm de diametro ,8,75 gramos
 #ALBARICOQUE: 55,7  mm de diámetro , 102,16 gramos
 
+    def clasificaciones(self):
+        cereza = [[26.98,8.75]]
+        numClusterC = self.modelo.predict(cereza)
+        print("Número de clúster de las cerezas: "+ str(numClusterC))
 
-cereza = [[26.98,8.75]]
-numCluster = modelo.predict(cereza)
-print("Número de clúster de las cerezas: "+ str(numCluster))
 
-
-albaricoque = [[55.7,102.16]]
-numCluster = modelo.predict(albaricoque)
-print("Número de clúster de los albaricoques: " + str(numCluster))
+        albaricoque = [[55.7,102.16]]
+        numClusterA = self.modelo.predict(albaricoque)
+        print("Número de clúster de los albaricoques: " + str(numClusterA))
 
 #Instrucciones a adaptar en función de los números de clústeres
 #determinados con anterioridad:
+    #Para saber cual de los dos cluster es el de cerezas y cual el de albaricoques
+    def busqueda(self):
+        cereza = [[26.98,8.75]]
+        numCluster = self.modelo.predict(cereza)
+        if int(numCluster)==0:
+            print("¡Es un albaricoque!")
+        else:
+            print("¡Es una cereza! ")
 
-cereza = [[26.98,8.75]]
-numCluster = modelo.predict(cereza)
-if int(numCluster)==0:
-    print("¡Es un albaricoque!")
-else:
-    print("¡Es una cereza! ")
 
-
-albaricoque = [[55.7,102.16]]
-numCluster = modelo.predict(albaricoque)
-if int(numCluster)==0:
-    print("¡Es un albaricoque!")
-else:
-    print("¡Es una cereza!")
+        albaricoque = [[55.7,102.16]]
+        numCluster = self.modelo.predict(albaricoque)
+        if int(numCluster)==0:
+            print("¡Es un albaricoque!")
+        else:
+            print("¡Es una cereza!")
 
 
 #---- Modelo de mezclas Gaussianas (GMM) -----------
-from sklearn import mixture
 
+    def Gauss(self):
 #Determinación de los clústeres (encontrar 2)
-gmm = mixture.GaussianMixture(n_components=2)
+        gmm = mixture.GaussianMixture(n_components=2)
 
-#Aprendizaje
-gmm.fit(frutas)
+        #Aprendizaje
+        gmm.fit(self.frutas)
 
-#Clasificación
-clusteres = gmm.predict(frutas)
+        #Clasificación
+        clusteres = gmm.predict(self.frutas)
 
-#Visualización de los clústeres
-plt.scatter(frutas.DIAMETRO, frutas.PESO, c=clusteres, s=40, cmap='viridis');
-plt.xlabel("DIAMETRO")
-plt.ylabel("PESO")
-plt.show()
+        #Visualización de los clústeres
+        plt.scatter(self.frutas.DIAMETRO, self.frutas.PESO, c=clusteres, s=40, cmap='viridis');
+        plt.xlabel("DIAMETRO")
+        plt.ylabel("PESO")
+        plt.show()
+    @staticmethod
+    def ejecutar():
+        apr=Aprendizaje()
+        #primer plot con los datos de las frutas
+        apr.vis()
+        #segundo plot, con los centroides
+        apr.aprKMeans()
+        apr.clasificaciones()
+        apr.busqueda()
+        #tercer plot
+        apr.Gauss()
 
+if __name__=="__main__":
+    aprendizaje=Aprendizaje()
+    aprendizaje.ejecutar()
